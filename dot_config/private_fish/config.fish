4423623328038efde2b9f73259e75e554b4638b5
Maple Wanderer <maplewanderer@pm.me>
2023-12-24 13:29:05 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,3 +3,15 @@ if status is-interactive
 end
 
 alias vi='vim'
+
+# the terminal rickroll
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+
+# Mocp must be launched with bash instead of Fish!
+alias mocp="bash -c mocp"
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
+# Or install it from the Arch User Repository: shell-color-scripts
+colorscript random
+
