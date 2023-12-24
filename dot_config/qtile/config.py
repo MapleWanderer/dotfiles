4ddd47fbc4f0b2b461e3e8bb6b0a2ac350031411
Maple Wanderer <maplewanderer@pm.me>
2023-12-24 16:21:29 +0000

    Update .config/qtile/config.py

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -88,9 +88,9 @@ keys = [
 
 groups = []
 group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9",]
-group_labels = ["WWW", "DEV", "SYS", "DOC", "GAME", "CHAT", "MUS", "VID", "GAY",]
+group_labels = ["WWW", "DEV", "SYS", "FILE", "GAME", "CHAT", "MUS", "VID", "GAY",]
 group_layouts = ["max", "max", "max", "max", "max", "max", "max", "max", "columns",]
-group_spawns = ["librewolf", "", "kitty", "pcmanfm", "", "discord", "", "", "kitty, kitty, kitty",]
+group_spawns = ["librewolf", "", "kitty", "pcmanfm", "", "discord", "", "", "kitty kitty kitty",]
 
 for i in range(len(group_names)):
     groups.append(
