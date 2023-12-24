f0af44207652d716349753de2f0373707f574861
Maple Wanderer <maplewanderer@pm.me>
2023-12-24 13:36:29 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,19 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+###########################
+### Maple's Fish Config ###
+###########################
 
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "vim"                 		  # $EDITOR use vim in terminal
+
+#Use vim instead of vi
 alias vi='vim'
+
+# switch between shells
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
 
 # the terminal rickroll
 alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
