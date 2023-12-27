ca2fa4407e7d0a156d49f6febdb00b0a3cbbed36
Maple Wanderer <maplewanderer@pm.me>
2023-12-27 01:07:24 +0000

    Update .config/qtile/autostart.sh

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 picom -b
 flameshot &
+redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
 #nm-applet &
