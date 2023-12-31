1efd5e05e1540f4512632f2a3370ca6403e28bcb
Maple Wanderer <maplewanderer@pm.me>
2023-12-31 14:02:09 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,15 +4,15 @@
 
 # Fish greeting
 function fish_greeting
-	random choice "Shouldn't have wished to live in more interesting times." "No traps, please." "No time to rest." "Swift as my feet will carry me." "At least things have stayed interesting." "These boots have seen everything." "All's well that ends... not as bad as it could have." "Cursed to put my hands on everything." "I've got a lot on my mind, and well... in it." "Still alive, despite everything." "I wish I had a Bag of Holding." "Is that blood? No, nevermind." "I wonder if the gods are watching me." "Never wanted the easy path." "Breathe deep, and move!" "Power courses through you. Authority."
+    random choice "Shouldn't have wished to live in more interesting times." "No traps, please." "No time to rest." "Swift as my feet will carry me." "At least things have stayed interesting." "These boots have seen everything." "All's well that ends... not as bad as it could have." "Cursed to put my hands on everything." "I've got a lot on my mind, and well... in it." "Still alive, despite everything." "I wish I had a Bag of Holding." "Is that blood? No, nevermind." "I wonder if the gods are watching me." "Never wanted the easy path." "Breathe deep, and move!" "Power courses through you. Authority."
 end
 
 ### EXPORT ###
-set TERM "xterm-256color"                         # Sets the terminal type
-export EDITOR="vim"                 		  # $EDITOR use vim in terminal
+set TERM xterm-256color # Sets the terminal type
+export EDITOR="nvim" # $EDITOR use vim in terminal
 
-#Use vim instead of vi
-alias vi='vim'
+#Use nvim instead of vi
+alias vi='nvim'
 
 # switch between shells
 alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
@@ -29,4 +29,3 @@ alias mocp="bash -c mocp"
 # Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
 # Or install it from the Arch User Repository: shell-color-scripts
 colorscript random
-
